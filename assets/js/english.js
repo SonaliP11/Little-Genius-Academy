30b91cd72f0fac590d99b06c2a3e380eb6b8b903
Pui Yi <bbycznkr@students.codeinstitute.net>
2024-12-16 13:41:30 +0000

    Level up message responsive styling

--- assets/js/english.js
+++ assets/js/english.js
@@ -295,7 +295,7 @@ function showLevelUpMessage(nextLevel) {
     const gameContainer = document.getElementById('game-container');
     if (gameContainer) {
         gameContainer.innerHTML += `
-            <div id="levelUpMessage" class="level-up-message">
+            <div id="levelUpMessage" class="level-up-message h2 col-10 col-md-5 col-lg-7 col-xl-9 p-5">
                 Excellent job, ${decodeURIComponent(userName)}!<br>Now let’s level up to ${nextLevel.charAt(0).toUpperCase() + nextLevel.slice(1)}!
             </div>
         `;
